e07376565dcce8989d751901b36564b516a5be18
Andrew Aliaj <andyaliaj@gmail.com>
2024-07-19 12:46:19 -0400

    testing

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-
+from new import taxtest;
 from new import sum; 
+from new import x;
 
 print(sum(1, 2));
 print("hello world");
+
+print(taxtest());
+
+
+def random(x):
+    x.append(6);
+    return x;
+
+print(random(x));
--- new.py
+++ new.py
@@ -2,3 +2,14 @@
 
 def sum (a, b):
     return a + b
+
+
+def taxtest():
+    print("")
+    
+
+x= [1, 2, 3, 4, 5]
+
+
+class Test: 
+    
